92d97b71f35e5e9c0dd3b30dfe50a1edece0fa93
kronifer <44979306+Kronifer@users.noreply.github.com>
2022-09-22 13:24:20 -0500

    feat: added support for multiple methods per route and added request body to request class

--- cosmo/request.py
+++ cosmo/request.py
@@ -5,9 +5,15 @@ class Request:
     """A class representing an HTTP request."""
 
     def __init__(
-        self, method: str, headers: dict, address: str, url_flags: Optional[dict] = None
+        self,
+        method: str,
+        headers: dict,
+        address: str,
+        url_flags: Optional[dict] = None,
+        body: Optional[str] = None,
     ):
         self.method = method
         self.headers = headers
         self.address = address
         self.url_flags = url_flags
+        self.body = body
--- cosmo/route.py
+++ cosmo/route.py
@@ -1,4 +1,5 @@
 import traceback
+from dataclasses import dataclass
 from typing import Coroutine
 
 from loguru import logger
@@ -6,26 +7,50 @@ from loguru import logger
 from .request import Request
 
 
+@dataclass
+class Subroute:
+    content_type: str
+    function: Coroutine
+
+
 class Route:
     """A class representing a route in a Cosmo app."""
 
     def __init__(self, path: str, method: str, content_type: str, function: Coroutine):
         self.path = path
-        self.method = method
+        self.method = method.upper()
         self.content_type = content_type
-        self.function = function
-        self.headers = {"Content-Type": self.content_type}
+        self.functions = {
+            "GET": None,
+            "POST": None,
+            "HEAD": None,
+            "PUT": None,
+            "DELETE": None,
+            "CONNECT": None,
+            "OPTIONS": None,
+            "TRACE": None,
+            "PATCH": None,
+        }
+        self.functions[method] = Subroute(content_type, function)
+        self.default_headers = {}
 
     async def _create_response(self, request: Request):
         try:
-            content = await self.function(request)
-            self.headers["Content-Length"] = len(content.content) + 2
+            method = request.method.upper()
+            subroute = self.functions.get(method, None)
+            if subroute is None:
+                return None
+            headers_dict = {"Content-Type": subroute.content_type}
+            content = await subroute.function(request)
+            headers_dict["Content-Length"] = len(content.content) + 2
             if content.headers is not None:
                 for header in content.headers:
-                    self.headers[header] = content.headers[header]
+                    headers_dict[header] = content.headers[header]
             headers = ""
-            for header in self.headers.keys():
-                headers += f"{header}: {self.headers[header]}\r\n"
+            for header in self.default_headers.keys():
+                headers += f"{header}: {self.default_headers[header]}\r\n"
+            for header in headers_dict.keys():
+                headers += f"{header}: {headers_dict[header]}\r\n"
             headers += "\r\n"
             if type(content.content) is bytes:
                 return (
